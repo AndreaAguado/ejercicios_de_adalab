f243607e614bc590dcbf8a4c966930c021d4a843
AndreaAguado <andrea95am@gmail.com>
2021-08-07 20:03:20 +0200

    upload exercises to practice swith

---
+++ modulo-2-leccion-03/modulo-2-leccion-03-ejercicio-switch/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const todayDate = 'Sábado';
+
+switch (todayDate) {
+  case 'Lunes':
+    console.log('Llorar mucho y muy fuertemente por la mañana');
+    break;
+  case 'Martes':
+    console.log('Hoy toca comer pasta');
+    break;
+  case 'Miércoles':
+    console.log('Estamos lejos del viernes pero más cerca que ayer');
+    break;
+  case 'Jueves':
+    console.log('Ya sólo queda un madrugón');
+    break;
+  case 'Viernes':
+    console.log('TGIF!!!!!!!!!!');
+    break;
+  case 'Sábado':
+    console.log('Descansar por la tarde');
+    break;
+  default:
+    console.log('Dormir hasta las 12');
+}
