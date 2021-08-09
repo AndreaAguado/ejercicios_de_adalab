466a3c6064db7529e12a9de00f460dd3eb973c32
AndreaAguado <andrea95am@gmail.com>
2021-08-09 19:13:33 +0200

    add exercise 2 module 2 lesson 4

---
+++ modulo-2-leccion-04/modulo-2-leccion-04-ejercicio-2/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function media (a, b, c, d){
+    return ( a + b + c +d )/4;
+}
+
+console.log(`La media es: ${media(1,1,1,1)}`);
+console.log(`La media es: ${media(1,4,8,3)}`);
+console.log(`La media es: ${media(100,42,33,56)}`);
