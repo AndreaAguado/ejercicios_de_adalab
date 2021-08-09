cc8368cff39f74f8661be088902dc462b0134149
AndreaAguado <andrea95am@gmail.com>
2021-08-09 19:03:19 +0200

    add exercise 1 module 2 lesson 4

---
+++ modulo-2-leccion-04/modulo-2-leccion-04-ejercicio-1/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function multiply (a,b) {
+    return a * b;
+}
+
+console.log(`El resultado es: ${multiply(8,9)}`);
+console.log(`El resultado es: ${multiply(9,5)}`);
+console.log(`El resultado es: ${multiply(8,7)}`);
