cea8d8d7d339c42a5d51929daa1041710f9b6b00
AndreaAguado <andrea95am@gmail.com>
2021-08-10 21:46:12 +0200

    add exercise 6

---
+++ modulo-2-leccion-05/modulo-2-leccion-05-ejercicio-06/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// const buttonElement = document.querySelector('.button');
+
+const input = document.querySelector('.input');
+const paragraph = document.querySelector('.paragraph');
+
+// // Función de ejemplo de los apuntes:
+// function handleButtonClick(event) {
+//     console.log(event.currentTarget);
+// }
+
+// buttonElement.addEventListener('click', handleButtonClick);
+
+//Funcion que recoge los inputs:
+
+function updateInfo(event){
+    // let aux=event.currentTarget.value;
+    paragraph.innerHTML=event.currentTarget.value;
+    console.log(event.currentTarget.value);
+}
+
+input.addEventListener('keydown', updateInfo);
+
