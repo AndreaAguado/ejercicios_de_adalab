613f67e8e38b4477b3066f86c3976479929f8f7f
AndreaAguado <andrea95am@gmail.com>
2021-08-12 19:21:11 +0200

    update exercise 5 arrays

--- modulo-2-leccion-07/modulo-2-leccion-07-ejercicio-05/js/main.js
+++ modulo-2-leccion-07/modulo-2-leccion-07-ejercicio-05/js/main.js
@@ -4,30 +4,29 @@
 let numbers = [20, 54, 76, 33, 100];
 
 let sum = 0;
-for (let i = 0; i < 4; i++ ) {
+for (let i = 0; i < 5; i++ ) {
     sum += numbers[i];
-    console.log(sum);
 }
 
 let media = sum / 5;
 
-console.log('Media =', media);
+console.log('Media de 5 =', media);
 
 // apartado b:
 
 numbers = [20, 54, 76, 33, 100, 1412];
 
 sum = 0;
-for (let i = 0; i < 5; i++ ) {
+for (let i = 0; i < 6; i++ ) {
     sum += numbers[i];
-    console.log(sum);
 }
 
 media = sum / 6;
 
-console.log('Media =', media);
+console.log('Media de 6 =', media);
 
 
+// apartado c:
 function average (numbers) {
     let sum = 0;
     let media;
@@ -35,7 +34,8 @@ function average (numbers) {
         sum += numbers[i];        
     }
 
-    media = Math.round(sum / numbers.length);
+    media = sum / numbers.length;
+    console.log('La media es: ' + media);
 }
 
 average(numbers);
