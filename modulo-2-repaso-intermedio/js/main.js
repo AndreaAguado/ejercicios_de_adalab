24a76b3d81f17ac30ebed66952f0d360ffb712d6
AndreaAguado <andrea95am@gmail.com>
2021-08-25 13:39:02 +0200

    add events to button update

--- modulo-2-repaso-intermedio/js/main.js
+++ modulo-2-repaso-intermedio/js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const updateButton = document.querySelector('.js_update');
+
+const smiley = document.querySelector('.js_title');
+
+const select = document.querySelector('.js_select');
+
+const body = document.querySelector('.body');
+
+function changeMood (){
+    smiley.innerHTML = select.value;
+    let number = getRandomInt(100);
+    if (number % 2 !== 0){
+        body.classList.toggle('odd');
+    }
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+  updateButton.addEventListener('click', changeMood);
