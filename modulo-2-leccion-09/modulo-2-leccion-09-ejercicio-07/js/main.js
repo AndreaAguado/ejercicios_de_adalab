70eb064bd7b1cdba465b842380e88d18f120254d
AndreaAguado <andrea95am@gmail.com>
2021-09-01 23:21:56 +0200

    add finished better christmas tree v.2

--- modulo-2-leccion-09/modulo-2-leccion-09-ejercicio-07/js/main.js
+++ modulo-2-leccion-09/modulo-2-leccion-09-ejercicio-07/js/main.js
@@ -1,27 +1,52 @@
 'use strict';
 
 function christmasTree (size) {
+    // let spaces = ' ';
+    // for (let index = 0; index < size; index++) {
+    //     spaces += ' ';        
+    // }    
+    let spaces = createSpacer(8);
+    let spaces2;
     let aux='▲';
     let top = '★';
     let trunk = '|';
     for (let index = 0; index < size; index++) {   
         if (index < 1) {
-            console.log(top);
+            //si esta en el primer elemento pone la estrella
+            console.log(spaces + top);     
         }    
         else if (index < 2) {
-            console.log(aux);
-
+            //si esta en el segundo elemento pone arbol
+            console.log(spaces + aux);
+            spaces2 = spaces.slice(0, -1);
         }
         else if (index === size-1){
-            console.log(trunk);
+            //si esta en el ultimo elemento pone tronco
+            console.log( spaces + trunk);           
         }
         else {
             aux = '▲'+ aux + '▲';
-            console.log(aux);
-        }
-
-        
+            // spaces2 = spaces2 - ' ';
+            spaces2 = spaces2.slice(0, -1);
+            console.log(spaces2 + aux);
+        }        
     }
 }
 
+function createSpacer (size){
+    let spaces = '';
+    for (let index = 0; index < size-1; index++) {
+        spaces += ' ';        
+    }
+    return spaces;
+}
+
 christmasTree(8);
+
+// function recortar (){
+//     let string = 'andrea';
+//     let string2 = string.slice(0, -1);
+//     console.log(string2);
+// }
+
+// recortar();
