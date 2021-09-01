fb2fcd84248bd3fd1f5b0c411c5adf6f54cbd3e4
AndreaAguado <andrea95am@gmail.com>
2021-09-01 20:00:51 +0200

    write just one task in the page

--- modulo-2-leccion-09/modulo-2-leccion-09-ejercicio-04/js/main.js
+++ modulo-2-leccion-09/modulo-2-leccion-09-ejercicio-04/js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const list = document.querySelector('.js_list');
+const subtitle = document.querySelector('.js_subtitle');
+
+const tasks = [
+    { name: 'Recoger setas en el campo', completed: true },
+    { name: 'Comprar pilas', completed: true },
+    { name: 'Poner una lavadora de blancos', completed: true },
+    {
+      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+      completed: false
+    }
+  ];
+
+  function writeTasks (tasks) {
+      list.innerHTML += `<li> ${tasks[0].name} </li>`;
+  }
+
+  writeTasks(tasks);
+
+  function updateSubtitle(tasks) {
+      subtitle.innerHTML = `Tienes ${ tasks.length} tareas: Y terminadas y Z por terminar`;
+  }
+
+  updateSubtitle(tasks);
