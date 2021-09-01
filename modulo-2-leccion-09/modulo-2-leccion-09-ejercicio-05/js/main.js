543ebf17dc3f4717cbdeda3869e9f10f25eb426f
AndreaAguado <andrea95am@gmail.com>
2021-09-01 22:25:05 +0200

    add christmas tree

---
+++ modulo-2-leccion-09/modulo-2-leccion-09-ejercicio-05/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function christmasTree (size) {
+    let aux='';
+    for (let index = 0; index < size; index++) {       
+        aux= aux+'▲';
+        console.log(aux);
+        
+    }
+}
+
+christmasTree(8);
