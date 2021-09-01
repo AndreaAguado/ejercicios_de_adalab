c842579b8ccfb48369a115bb6c60803c5a8d1806
AndreaAguado <andrea95am@gmail.com>
2021-09-01 19:03:56 +0200

    add exercise 1

---
+++ modulo-2-leccion-09/modulo-2-leccion-09-ejercicio-01/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function get100numbers() {
+    let array = [];
+    for (let index = 0; index < 100; index++) {
+        array.push(index + 1);        
+    }
+    console.log(array);
+}
+
+get100numbers();
