db6942a9529c9edec51a95f43b0931b3c3fbda33
AndreaAguado <andrea95am@gmail.com>
2021-09-03 22:17:05 +0200

    save input value in local storage

--- modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-04/js/main.js
+++ modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-04/js/main.js
@@ -5,6 +5,15 @@ const paragraph = document.querySelector('.js-text');
 
 function handleInput() {
     paragraph.innerHTML = input.value;
+    localStorage.setItem('name', input.value);
 }
+
+function hasContent () {
+    paragraph.innerHTML= localStorage.getItem('name');
+    input.value = localStorage.getItem('name');
+}
+
+hasContent();
+
 
 input.addEventListener('keyup', handleInput);
