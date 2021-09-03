5213043dd0c72fee287c40b8a3b5c2a78e4554c4
AndreaAguado <andrea95am@gmail.com>
2021-09-03 21:16:32 +0200

    sw browser shows all the pages when button is clicked

--- modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-02/js/main.js
+++ modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-02/js/main.js
@@ -8,8 +8,14 @@ const button = document.querySelector('.js-submit');
 
 const list = document.querySelector('.js-char-list');
 
-function handleButton () {
-    const url = urlMaker(input.value);
+const buttonNext = document.querySelector('.js-button');
+
+
+
+
+function handleButton () {    
+    let url = urlMaker(input.value);
+    console.log(url);
     fetch(url)
     .then(response => response.json())
     .then(characters => {
@@ -18,21 +24,56 @@ function handleButton () {
         if(characters.count > 0) {
             list.innerHTML = '';
             for (let i = 0; i < people.length; i++) {
-            list.innerHTML += `<li class="list-item">${people[i].name} : ${people[i].gender}</li>`;          
+            list.innerHTML += `<li class="list-item">${people[i].name} : ${people[i].gender}</li>`;              
         }
         }
         else {
             list.innerHTML = `<li class="list-item">No se encontraron resultados</li>`; 
         }
+        if(characters.next !== null){
+            buttonNext.classList.remove('hidden');
+        }
+    })
+}
+
+let page = 2;
+
+function handleButton2 () {
+    console.log('estoy aqui');
+    let url = addNextPage(input.value, page);
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(characters => {
+        console.log(characters);
+        console.log(characters.next);
+        const people = characters.results;
+            // list.innerHTML = '';
+            for (let i = 0; i < people.length; i++) {
+            list.innerHTML += `<li class="list-item">${people[i].name} : ${people[i].gender}</li>`;     
+        }
+        if(characters.next !== null){
+            page++;
+        }
+        else {
+            buttonNext.classList.add('hidden');
+        }
 
     })
-  
-
+    
 }
 
 button.addEventListener('click', handleButton);
 
+buttonNext.addEventListener('click', handleButton2);
+
 function urlMaker(input){
     const url = 'https://swapi.dev/api/people/?search=' + input ;
+    return url;
+}
+
+function addNextPage(input, page){
+    let urlBeggining = urlMaker(input);
+    let url = urlBeggining + '&page=' + page;
     return url;
 }
