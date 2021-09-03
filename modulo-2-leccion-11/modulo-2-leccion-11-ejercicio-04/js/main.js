939e6ee9a1fda37488e5d52b7bf22e0b4883c9e3
AndreaAguado <andrea95am@gmail.com>
2021-09-03 22:08:30 +0200

    add handle to js

--- modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-04/js/main.js
+++ modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-04/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const input = document.querySelector('.js-input');
+const paragraph = document.querySelector('.js-text');
+
+function handleInput() {
+    paragraph.innerHTML = input.value;
+}
+
+input.addEventListener('keyup', handleInput);
