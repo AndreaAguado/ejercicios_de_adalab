36ac5a029eebcaa399d7bd7b1452970fe3fd2ebd
AndreaAguado <andrea95am@gmail.com>
2021-09-03 21:59:29 +0200

    fix ex 5 lesson 5

--- modulo-2-leccion-05/modulo-2-leccion-05-ejercicio-05/js/main.js
+++ modulo-2-leccion-05/modulo-2-leccion-05-ejercicio-05/js/main.js
@@ -3,16 +3,19 @@
 const background = document.querySelector('.main');
 
 
-function changeColor (){
-    console.log(`inside the function`);
+function changeColor (event){
+if(event.key === 'r'){
     if(background.classList.contains('purple')){
         background.classList.remove('purple');
         background.classList.add('red');
     }
-    else if (background.classList.contains('red')){
+}
+else if (event.key === 'm'){
+    if (background.classList.contains('red')){
         background.classList.remove('red');
         background.classList.add('purple');
     }
+}
 }
 
 document.addEventListener('keydown', changeColor);
