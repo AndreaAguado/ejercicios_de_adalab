87daa8f81eff3f4e9015c254eedc969baf8113f5
AndreaAguado <andrea95am@gmail.com>
2021-09-04 18:23:43 +0200

    add ex 7 folders

---
+++ modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-07/js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const inputs = document.querySelectorAll('.js-input');
+const paragraph = document.querySelector('.js-text');
+
+function handleInput(event) {
+    console.log('estoy aqui');
+    let written = event.currentTarget.value;
+    let userData = [];
+    userData.push(inputs[0].value, inputs[1].value);
+    console.log(userData);
+    paragraph.innerHTML += written + ' ';
+
+    localStorage.setItem('userData', JSON.stringify(userData));
+}
+
+function hasContent () {
+    const data = JSON.parse(localStorage.getItem('userData'));
+    console.log(data);
+    inputs[0].value = data[0];
+    inputs[1].value = data[1];
+    paragraph.innerHTML = data[0] + ' ' + data[1];
+}
+
+hasContent();
+
+for (const input of inputs) {
+    input.addEventListener('change', handleInput);
+}
+
