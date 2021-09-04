dc2d26208d059ff008cdda2529b5c8bee32d335f
AndreaAguado <andrea95am@gmail.com>
2021-09-04 20:49:14 +0200

    make petition to get kittens

--- modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-10/js/main.js
+++ modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-10/js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const image = document.querySelector('.img');
+
+fetch('https://api.thecatapi.com/v1/images/search?format=json', {
+  headers: {
+    "x-api-key": "c33dc4111cbf4beb813929ee97a3f362",
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    showImage(data);
+
+  });
+
+  function showImage(data) {
+      image.setAttribute('src',data[0].url);
+  }
