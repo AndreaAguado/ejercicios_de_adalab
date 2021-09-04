3198d6c009d4ace21eba937cf7961ebb46c2ccf9
AndreaAguado <andrea95am@gmail.com>
2021-09-04 18:22:06 +0200

    save values in array in localStorage

--- modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-06/js/main.js
+++ modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-06/js/main.js
@@ -1,19 +1,30 @@
 'use strict';
 
-const input = document.querySelector('.js-input');
+const inputs = document.querySelectorAll('.js-input');
 const paragraph = document.querySelector('.js-text');
 
-function handleInput() {
-    paragraph.innerHTML = input.value;
-    localStorage.setItem('name', input.value);
+function handleInput(event) {
+    console.log('estoy aqui');
+    let written = event.currentTarget.value;
+    let userData = [];
+    userData.push(inputs[0].value, inputs[1].value);
+    console.log(userData);
+    paragraph.innerHTML += written + ' ';
+
+    localStorage.setItem('userData', JSON.stringify(userData));
 }
 
 function hasContent () {
-    paragraph.innerHTML= localStorage.getItem('name');
-    input.value = localStorage.getItem('name');
+    const data = JSON.parse(localStorage.getItem('userData'));
+    console.log(data);
+    inputs[0].value = data[0];
+    inputs[1].value = data[1];
+    paragraph.innerHTML = data[0] + ' ' + data[1];
 }
 
 hasContent();
 
+for (const input of inputs) {
+    input.addEventListener('change', handleInput);
+}
 
-input.addEventListener('keyup', handleInput);
