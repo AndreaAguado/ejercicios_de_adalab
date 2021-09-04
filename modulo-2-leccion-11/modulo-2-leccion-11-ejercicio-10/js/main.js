cd8c1ec0e7a461357f48eab5786984aadd082493
AndreaAguado <andrea95am@gmail.com>
2021-09-04 21:09:51 +0200

    comment function

--- modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-10/js/main.js
+++ modulo-2-leccion-11/modulo-2-leccion-11-ejercicio-10/js/main.js
@@ -2,6 +2,8 @@
 
 const image = document.querySelector('.js-img');
 
+const button = document.querySelector('.js-button');
+
 function requestKittens() {
     fetch('https://api.thecatapi.com/v1/images/search?format=json', {
   headers: {
@@ -15,7 +17,28 @@ function requestKittens() {
 
   });
 }
+
+// function asd(){
+//     fetch('https://images.abstractapi.com/v1/', {
+//         headers: {
+//           "x-api-key": "c33dc4111cbf4beb813929ee97a3f362",
+//         }
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(data);
+//           showImage(data);
+      
+//         });
+// }
+// asd();
 requestKittens();
 function showImage(data) {
     image.setAttribute('src',data[0].url);
 }
+
+function handleButton(){
+    requestKittens();
+}
+
+button.addEventListener('click',handleButton);
