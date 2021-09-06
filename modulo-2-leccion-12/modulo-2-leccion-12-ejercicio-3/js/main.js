5413629c04ddce365ce1d9591f242cc693c7c5a8
AndreaAguado <andrea95am@gmail.com>
2021-09-06 21:41:40 +0200

    get the names in the selector from js

--- modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-3/js/main.js
+++ modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-3/js/main.js
@@ -1,1 +1,58 @@
 'use strict';
+
+const select = document.querySelector('#data');
+console.log(select);
+
+const option1 = document.createElement('option');
+const option2 = document.createElement('option');
+const option3 = document.createElement('option');
+console.log(option1);
+
+const personalData = [
+    { firstName: 'Mikasa',
+    lastName: 'Ackerman' ,
+    phone: '000111222'
+    },
+    { firstName: 'Armin',
+    lastName: 'Arlert' ,
+    phone: '999888000'
+    },
+    { firstName: 'Eren',
+    lastName: 'Jaeger' ,
+    phone: '99777888'
+    }
+]
+console.log(personalData);
+
+function fillOptions(){
+    let newContent;
+    for (let i = 0; i < personalData.length; i++) {
+        if(i===0){
+            newContent = document.createTextNode(personalData[0].firstName);
+            console.log(newContent);
+            option1.appendChild(newContent);
+            option1.setAttribute('value', newContent);
+            select.appendChild(option1);
+        }
+        else if(i===1){
+            newContent = document.createTextNode(personalData[1].firstName);
+            option2.appendChild(newContent);
+            option2.value=newContent;
+            select.appendChild(option2);
+        }
+        else if(i===2){
+            newContent = document.createTextNode(personalData[2].firstName);
+            option3.appendChild(newContent);
+            option3.value=newContent;
+            select.appendChild(option3);
+        } 
+        
+    }
+}
+
+fillOptions();
+
+function fillForm(ev){
+
+
+}
