8c41b951d6e46bbd771e098c974aa7f179d5fac0
AndreaAguado <andrea95am@gmail.com>
2021-09-06 19:46:28 +0200

    ad ex 1

--- modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-1/js/main.js
+++ modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-1/js/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const numbers = [1, 2, 3];
+const list = document.querySelector('.js-list');
+const newElement1 = document.createElement('li');
+const newElement2 = document.createElement('li');
+const newElement3 = document.createElement('li');
+
+
+// for (let i = 0; i < numbers.length; i++) {
+//    const newContent= document.createTextNode(numbers[i]);
+//    newElement.appendChild(newContent);
+//    console.log(newElement);
+//    list.appendChild(newElement);    
+// }
+
+for (let i = 0; i < numbers.length; i++) {
+    let newContent;
+    if(i===1){
+        newContent= document.createTextNode(numbers[i]);
+        newElement1.appendChild(newContent);
+        console.log(newElement1);
+        list.appendChild(newElement1); 
+    }
+    else if(i===2){
+        newContent= document.createTextNode(numbers[i]);
+        newElement2.appendChild(newContent);
+        console.log(newElement2);
+        list.appendChild(newElement2); 
+    }
+    else {
+        newContent= document.createTextNode(numbers[i]);
+        newElement3.appendChild(newContent);
+        console.log(newElement3);
+        list.appendChild(newElement3); 
+    }
+   
+ }
