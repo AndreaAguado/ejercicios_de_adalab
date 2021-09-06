8f6b2044a749e96a2755f3c2ced0c44e31c9098c
AndreaAguado <andrea95am@gmail.com>
2021-09-06 21:02:24 +0200

    add styles

--- modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-2/js/main.js
+++ modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-2/js/main.js
@@ -3,7 +3,7 @@
 const madrid = ['https://traverology.media/wp-content/uploads/2019/06/madrid-para-ellas-tvr-1900x1163.jpg', 'https://i.pinimg.com/originals/a8/d9/a6/a8d9a624315393b7d2d7686c21d76553.jpg' , 'https://distritocastellananorte.com/wp-content/uploads/2019/09/madrid-de-los-austrias-plaza-mayor-centro-turismo.jpg'];
 
 const paris = ['https://i.pinimg.com/originals/9f/52/ea/9f52eaf927292b3958827ea20781272b.jpg', 'https://cdn2.civitatis.com/francia/paris/guia/catedral-notre-dame-grid-m.jpg', 'https://www.isango.com/theguidebook/wp-content/uploads/2017/03/header2-3.jpg'];
-const ny = ['https://i.pinimg.com/736x/46/35/39/463539dc91b02884dd59cc67da6efb89.jpg' , 'https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/241211129_5083682728315524_2945423912376838272_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=TLkM-LjsmdsAX8tY2pN&_nc_ht=scontent-mad1-1.xx&oh=4397068a92d040c98f53c16511fe9079&oe=613BC536' , 'https://www.metalocus.es/sites/default/files/files/metalocus_times-square_new-york_snohetta_09.jpg'];
+const ny = ['https://i.pinimg.com/736x/46/35/39/463539dc91b02884dd59cc67da6efb89.jpg' , 'https://www.metalocus.es/sites/default/files/files/metalocus_times-square_new-york_snohetta_09.jpg','https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/241211129_5083682728315524_2945423912376838272_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=TLkM-LjsmdsAX8tY2pN&_nc_ht=scontent-mad1-1.xx&oh=4397068a92d040c98f53c16511fe9079&oe=613BC536'];
 
 const input = document.querySelector('#city');
 console.log(input);
