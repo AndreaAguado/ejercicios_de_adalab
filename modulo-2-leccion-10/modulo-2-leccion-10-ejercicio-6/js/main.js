f569032486af6276719f151c6c77b2b1c086f84c
AndreaAguado <andrea95am@gmail.com>
2021-09-06 09:07:15 +0200

    add function to request dog breeds

--- modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-6/js/main.js
+++ modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-6/js/main.js
@@ -1,16 +1,14 @@
 'use strict';
 
-fetch('https://rand.fun/number/integer?max=')
-.then( response => response.json())
-.then (randomNumber => {
-    
-})
 
-fetch ('https://dog.ceo/api/breeds/list/all')
-.then( response => response.json())
-.then( dogBreeds => {
-    const breeds = dogBreeds.message;
-    return fetch ('https://dog.ceo/api/breed/' + breeds[randomNumber] + '/images/random')
+function requestToDogAPI(){
+    fetch ('https://dog.ceo/api/breeds/list/all')
+    .then( response => response.json())
+    .then( dogBreeds => {
+        const breeds = dogBreeds.message;
+        console.log(breeds);
+    
+    })
 }
-  
-    )
+
+requestToDogAPI();
