8b15924545dff2507b0e1d2fac6d58869ed5c2bf
AndreaAguado <andrea95am@gmail.com>
2021-09-07 18:14:01 +0200

    add ex 3

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-02/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-02/js/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const users = [
+    { name: 'María', isPremium: false },
+    { name: 'Lucía', isPremium: true },
+    { name: 'Susana', isPremium: true },
+    { name: 'Rocío', isPremium: false },
+    { name: 'Inmaculada', isPremium: false }
+  ];
+
+  const premiumGreeting = (user) => {
+      let greeting = '';
+      if (user.isPremium){
+        greeting = `Bienvenida ${user.name}. Gracias por confiar en nosotros.`
+      }
+      else {
+        greeting = `Bienvenida ${user.name}.`
+      }
+      return greeting;
+  }
+
+  const greetings = users.map(premiumGreeting);
+  console.log(greetings);
