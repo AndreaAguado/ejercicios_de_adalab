e7193f1a6f797123f99763994e23dd019860b1e5
AndreaAguado <andrea95am@gmail.com>
2021-09-07 17:57:00 +0200

    add ex 1

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-01/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-01/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const marks = [5, 4, 6, 7, 9];
+
+//inflatedMarks is only applied to 1 vble at once. When given to map, map applies inflatedMarks to each and every element of the array, that's why inflatedMarks DOES NOT work with a for:
+
+const inflatedMarks = (mark) => mark +1; 
+
+const newMarks = marks.map(inflatedMarks);
+console.log(newMarks);
