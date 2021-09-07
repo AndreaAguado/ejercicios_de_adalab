1a0d32b89aca86fd17bd0c5c177a2049f8e41006
AndreaAguado <andrea95am@gmail.com>
2021-09-07 09:55:53 +0200

    fill form

--- modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-3/js/main.js
+++ modulo-2-leccion-12/modulo-2-leccion-12-ejercicio-3/js/main.js
@@ -6,7 +6,8 @@ console.log(select);
 const option1 = document.createElement('option');
 const option2 = document.createElement('option');
 const option3 = document.createElement('option');
-console.log(option1);
+
+const input = document.querySelectorAll('.js-input');
 
 const personalData = [
     { firstName: 'Mikasa',
@@ -25,25 +26,30 @@ const personalData = [
 console.log(personalData);
 
 function fillOptions(){
-    let newContent;
+    // let newContent;
     for (let i = 0; i < personalData.length; i++) {
         if(i===0){
-            newContent = document.createTextNode(personalData[0].firstName);
-            console.log(newContent);
-            option1.appendChild(newContent);
-            option1.setAttribute('value', newContent);
+            const newContent1 = document.createTextNode(personalData[0].firstName);
+            // console.log(newContent1);
+            // option1.value = newContent1;
+            option1.appendChild(newContent1);
+            option1.setAttribute('value', personalData[0].firstName);
             select.appendChild(option1);
         }
         else if(i===1){
-            newContent = document.createTextNode(personalData[1].firstName);
-            option2.appendChild(newContent);
-            option2.value=newContent;
+            const newContent2 = document.createTextNode(personalData[1].firstName);
+            console.log(typeof newContent2);
+            // option2.value = newContent2;
+            option2.appendChild(newContent2);            
+            option2.setAttribute('value', personalData[1].firstName);
             select.appendChild(option2);
         }
         else if(i===2){
-            newContent = document.createTextNode(personalData[2].firstName);
-            option3.appendChild(newContent);
-            option3.value=newContent;
+            const newContent3 = document.createTextNode(personalData[2].firstName);
+            // console.log(newContent3);
+            option3.appendChild(newContent3);
+            // option3.value=newContent3;
+            option3.setAttribute('value', personalData[2].firstName);
             select.appendChild(option3);
         } 
         
@@ -53,6 +59,30 @@ function fillOptions(){
 fillOptions();
 
 function fillForm(ev){
-
-
+    console.log('aqui');
+    const selected = ev.target;
+    console.log(selected);
+    console.dir(selected);
+   
+    if(selected.value === personalData[0].firstName ){
+        input[0].value = personalData[0].firstName;
+        input[1].value = personalData[0].lastName;
+        input[2].value = personalData[0].phone;
+    }  
+    else if(selected.value === personalData[1].firstName ){
+        input[0].value = personalData[1].firstName;
+        input[1].value = personalData[1].lastName;
+        input[2].value = personalData[1].phone;
+    } 
+    else if(selected.value === personalData[2].firstName ){
+        input[0].value = personalData[2].firstName;
+        input[1].value = personalData[2].lastName;
+        input[2].value = personalData[2].phone;
+    } 
+    
 }
+
+select.addEventListener('change',fillForm);
+option1.addEventListener('change',fillForm);
+option2.addEventListener('change',fillForm);
+option3.addEventListener('change',fillForm);
