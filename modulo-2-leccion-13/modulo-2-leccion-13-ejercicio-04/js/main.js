a3844c436e7534e8563cc98d48afaaaabe2dc816
AndreaAguado <andrea95am@gmail.com>
2021-09-07 18:34:48 +0200

    add ex 4

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-04/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-04/js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const users = [
+    { name: 'María', isPremium: false },
+    { name: 'Lucía', isPremium: true },
+    { name: 'Susana', isPremium: true },
+    { name: 'Rocío', isPremium: false },
+    { name: 'Inmaculada', isPremium: false }
+  ];
+
+
+  const isPremium = (user) => {
+      let vip;
+      if(user.isPremium) {
+          vip = user;
+      }
+      return vip;
+  }
+  const premiumOnes = users.filter(isPremium);
+  console.log(premiumOnes);
