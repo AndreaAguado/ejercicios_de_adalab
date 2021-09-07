644e14ed8cbf9feab7c4a573569d0c9c99a3b0af
AndreaAguado <andrea95am@gmail.com>
2021-09-07 20:54:47 +0200

    add ex 10 part b

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-10/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-10/js/main.js
@@ -8,5 +8,14 @@ const users = [
     { name: 'Inmaculada', isPremium: false, pin: 8998 }
   ];
 
+
+//   Buscar usuaria cuyo pin=5232
   const problematic = users.find((users) => users.pin === 5232); 
   console.log(problematic);
+
+//   Borrar usuaria cuyo pin=5232
+    const dropoutIndex = users.findIndex((users) => users.pin === 5232);
+    const newUsers = users.splice(dropoutIndex,1);
+    console.log(users);
+
+
