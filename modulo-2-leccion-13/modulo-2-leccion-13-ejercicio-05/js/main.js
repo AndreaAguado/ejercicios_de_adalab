b1b215b4d903f174c8a8b883111cc4f36221fdc1
AndreaAguado <andrea95am@gmail.com>
2021-09-07 18:42:17 +0200

    add ex 5

--- modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-05/js/main.js
+++ modulo-2-leccion-13/modulo-2-leccion-13-ejercicio-05/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const pins = [2389, 2384, 2837, 5232, 8998];
+
+
+const findEven = (number) => number % 2 === 0;
+
+const evenOnes = pins.filter(findEven);
+
+console.log(evenOnes);
