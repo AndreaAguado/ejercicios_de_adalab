c818c29c7895f2d15f54f25ff6fe6187f8ca0272
AndreaAguado <andrea95am@gmail.com>
2021-09-08 21:05:41 +0200

    add ex 2

--- modulo-2-leccion-14/modulo-2-leccion-14-ejercicio-2/js/main.js
+++ modulo-2-leccion-14/modulo-2-leccion-14-ejercicio-2/js/main.js
@@ -2,11 +2,35 @@
 
 let counter = 0;
 
-const page = document.querySelector('js-body');
+const page = document.querySelector('.js-body');
+const title = document.querySelector('.js-title');
+const container = document.querySelector('.js-container');
+
+let html = '';
 const incrementAndShowCounter = () => {
-  counter++;
   const time = document.querySelector('.js-time');
-  time.innerHTML = counter;
+  if (counter < 12 ){
+    paintGrape();
+    counter++;    
+    time.innerHTML = counter;   
+  }
+  else{
+    time.classList.add('hidden');
+    html = `<img class="img js-img hidden" src="./images/uva.png" alt="uva">`;
+    container.innerHTML = html;
+    celebration();
+  }
 };
 
+function paintGrape(){
+  if(counter < 12 ){ 
+    html += `<img class="img js-img" src="./images/uva.png" alt="uva">`;
+    container.innerHTML = html;
+  }
+}
+
+function celebration(){
+  page.classList.add('bg');
+  title.classList.remove('hidden');
+}
 setInterval(incrementAndShowCounter, 1000);
