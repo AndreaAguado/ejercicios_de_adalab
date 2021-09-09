0255683cb50a75cd5aebfb94f1d22ca47e6ecb2c
AndreaAguado <andrea95am@gmail.com>
2021-09-09 11:52:25 +0200

    display movie list in html

--- modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-ghibli/js/main.js
+++ modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-ghibli/js/main.js
@@ -1,13 +1,18 @@
 'use strict';
 
-// const output = document.querySelector('.js_text');
+const list = document.querySelector('.js_list');
 
 fetch('https://ghibliapi.herokuapp.com/films/')
 .then(response => response.json())
 .then(data =>{
     console.log(data);
+    let html= '';
     for (const movie of data) {
         console.log(movie.title);
+        html += ` <li class="list-item js_list_item">`;
+        html += movie.title;
+        html += `</li>`;
     }  
+    list.innerHTML += html;
 })
 
