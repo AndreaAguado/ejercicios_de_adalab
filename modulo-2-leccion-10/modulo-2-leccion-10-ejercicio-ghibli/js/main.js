2ab60a1dc479322b189cdac61700131bb23b841a
AndreaAguado <andrea95am@gmail.com>
2021-09-09 13:05:24 +0200

    create separate function to display list

--- modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-ghibli/js/main.js
+++ modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-ghibli/js/main.js
@@ -1,18 +1,34 @@
 'use strict';
 
 const list = document.querySelector('.js_list');
+const button = document.querySelector('.js_button');
 
+let movies = [];
+let data = [];
 fetch('https://ghibliapi.herokuapp.com/films/')
 .then(response => response.json())
 .then(data =>{
-    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        movies[i] = data[i];       
+    }
+    console.log(movies);
+    displayList();
+})
+
+function displayList(){
     let html= '';
-    for (const movie of data) {
+    for (const movie of movies) {     
         console.log(movie.title);
         html += ` <li class="list-item js_list_item">`;
         html += movie.title;
         html += `</li>`;
     }  
     list.innerHTML += html;
-})
+    console.log(movies.title);
+}
+function handleButton(){
+
+}
+
+button.addEventListener('click',handleButton);
 
