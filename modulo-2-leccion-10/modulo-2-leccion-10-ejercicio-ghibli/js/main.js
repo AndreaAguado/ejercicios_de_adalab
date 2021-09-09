481bd1e6ce78a80a70f0250648df812b39c3bf68
AndreaAguado <andrea95am@gmail.com>
2021-09-09 11:36:09 +0200

    make petition to api

--- modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-ghibli/js/main.js
+++ modulo-2-leccion-10/modulo-2-leccion-10-ejercicio-ghibli/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// const output = document.querySelector('.js_text');
+
+fetch('https://ghibliapi.herokuapp.com/films/')
+.then(response => response.json())
+.then(data =>{
+    console.log(data);
+    for (const movie of data) {
+        console.log(movie.title);
+    }  
+})
+
