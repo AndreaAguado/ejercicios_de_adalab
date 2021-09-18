28631b2954ca9b945b8c862e70ec5334024bbb38
AndreaAguado <andrea95am@gmail.com>
2021-09-18 19:56:07 +0200

    add ex 2.4.3 menu burger

--- modulo-3-leccion-02/modulo-3-leccion-02.4-ejercicio-03/src/components/App.js
+++ modulo-3-leccion-02/modulo-3-leccion-02.4-ejercicio-03/src/components/App.js
@@ -1,9 +1,36 @@
-import '../styles/App.css';
+import '../styles/App.scss';
+import {useState} from 'react';
 
 function App() {
+  const[state,setState] = useState('hidden');
+  const handleMenu = (ev) => {
+    ev.preventDefault();
+    if(state === 'hidden'){
+      setState('');
+    }
+    else{
+      setState('hidden');
+    }
+  }
+
+  const menuClasses = () => {
+      let myClass = `menu ${state}`;
+      return myClass;
+  }
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <header>
+        <nav><a href="."><i onClick={handleMenu} className="fas fa-bars"></i></a></nav>
+        <div className={menuClasses()}>
+         <div className="menu__contents">
+           <a href=".">Link 1</a>
+           <a href=".">Link 2</a>
+           <a href=".">Link 3</a>
+         </div>
+         <i onClick={handleMenu} className="far fa-times-circle menu__icon"></i>
+        </div>
+      </header>
+
     </div>
   );
 }
