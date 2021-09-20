228f20ad1e1b9b83c8077da80cfc064c59fde12d
AndreaAguado <andrea95am@gmail.com>
2021-09-20 18:56:11 +0200

    add form

--- modulo-3-leccion-03/modulo-3-leccion-03.1-ejercicio-1/src/components/App.js
+++ modulo-3-leccion-03/modulo-3-leccion-03.1-ejercicio-1/src/components/App.js
@@ -3,7 +3,15 @@ import '../styles/App.css';
 function App() {
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <h1>Selecciona los ingredientes de la tortilla de patatas:</h1>
+      <form className="form" action="">
+        <label htmlFor="ingredient1"><input type="checkbox" name="ingredient1" id="ingredient1" />Macarrones</label>
+        <label htmlFor="ingredient2"><input type="checkbox" name="ingredient2" id="ingredient2" />Patatas</label>
+        <label htmlFor="ingredient3"><input type="checkbox" name="ingredient3" id="ingredient3" />Nueces</label>
+        <label htmlFor="ingredient4"><input type="checkbox" name="ingredient4" id="ingredient4" />Huevos</label>
+        <label htmlFor="ingredient5"><input type="checkbox" name="ingredient5" id="ingredient5" />Cebolla</label>
+        <label htmlFor="ingredient6"><input type="checkbox" name="ingredient6" id="ingredient6" />Cerveza</label>
+      </form>
     </div>
   );
 }
