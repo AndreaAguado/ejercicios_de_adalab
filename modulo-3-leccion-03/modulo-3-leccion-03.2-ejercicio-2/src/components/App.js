ccc904f65b0bb1a5be41d7bebfd0996526c88c26
AndreaAguado <andrea95am@gmail.com>
2021-09-20 21:50:34 +0200

    add ex 3.2.2

--- modulo-3-leccion-03/modulo-3-leccion-03.2-ejercicio-2/src/components/App.js
+++ modulo-3-leccion-03/modulo-3-leccion-03.2-ejercicio-2/src/components/App.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 const App = () => {
   const [numberA, setNumberA] = useState(0);
   const [numberB, setNumberB] = useState(0);
+  const [operation, setOperation] = useState('Sumar');
 
   const handleNumberA = ev => {
     setNumberA(ev.target.value);
@@ -15,24 +16,69 @@ const App = () => {
     setNumberB(ev.target.value);
   };
 
-  const total = parseInt(numberA) + parseInt(numberB);
+  const handleSelector = (ev) => {
+    setOperation(ev.target.value);
+  }
+  const operationChosen = () => {
+    if(operation === 'Sumar'){
+      return 'suma';
+    }
+    else if(operation === 'Restar'){
+      return 'resta';
+    }
+    else if(operation === 'Multiplicar'){
+      return 'multiplicación';
+    }
+    else if(operation === 'Dividir'){
+      return 'división';
+    }
+  }
+  const total = () => {
+    if(operation === 'Sumar'){
+      return parseInt(numberA) + parseInt(numberB);
+    }
+    else if(operation === 'Restar'){
+      return parseInt(numberA) - parseInt(numberB);
+    }
+    else if(operation === 'Multiplicar'){
+      return parseInt(numberA) * parseInt(numberB);
+    }
+    else if(operation === 'Dividir'){
+      return parseInt(numberA) / parseInt(numberB);
+    }
+  } 
+
+  const reset = (ev) => {
+    ev.preventDefault();
+    setNumberA(0);
+    setNumberB(0);
+    setOperation('Sumar');
+  }
 
   return (
-    <div>
+    <div className="page">
       <h1>La calculadora:</h1>
-      <form>
+      <form className="form">
+        <label htmlFor="operation">Escoge la operación:</label>
+        <select onChange={handleSelector} className="selector" name="operation" id="operation">
+          <option value="Sumar">Sumar</option>
+          <option value="Restar">Restar</option>
+          <option value="Multiplicar">Multiplicar</option>
+          <option value="Dividir">Dividir</option>
+        </select>
         <label>
           Escribe un número:
-          <input type="number" name="name" onChange={handleNumberA} />
+          <input className="input" value={numberA} type="number" name="name" onChange={handleNumberA} />
         </label>
         <label>
           Escribe otro número:
-          <input type="number" name="email" onChange={handleNumberB} />
+          <input className="input" value={numberB} type="number" name="email" onChange={handleNumberB} />
         </label>
+        <button className="button" onClick={reset}>Reset</button>
       </form>
       <p>
-        La suma de <strong>{numberA}</strong> y <strong>{numberB}</strong> es
-        <strong> {total}</strong>.
+        La <strong> {operationChosen()}</strong> de <strong>{numberA}</strong> y <strong>{numberB}</strong> es
+        <strong> {total()}</strong>.
       </p>
     </div>
   );
