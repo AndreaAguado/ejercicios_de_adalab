39cc4a5f6397c290e44bed0b03b9e9355d8f455e
AndreaAguado <andrea95am@gmail.com>
2021-09-20 20:22:59 +0200

    add ex 3.1.1

--- modulo-3-leccion-03/modulo-3-leccion-03.1-ejercicio-2/src/components/App.js
+++ modulo-3-leccion-03/modulo-3-leccion-03.1-ejercicio-2/src/components/App.js
@@ -1,9 +1,19 @@
 import '../styles/App.css';
-
+import {useState} from 'react';
 function App() {
+const [text, setText] = useState('');
+  const translateText = (ev) => {
+    let text = ev.target.value;
+    return setText(text.replace(/[aeiou]/gi, 'i'));    
+  }
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <h1>Traductor MIMIMI</h1>
+      <form className="form" action="">
+        <label htmlFor="toBeTranslated">¿Qué quieres traducir?</label>
+        <textarea onChange={translateText} name="toBeTranslated" id="toBeTranslated" cols="30" rows="10"></textarea>
+      </form>
+      <p>{text}</p>
     </div>
   );
 }
