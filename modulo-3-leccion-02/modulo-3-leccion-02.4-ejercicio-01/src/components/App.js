fde13ef3daa5d417998a627840214f833c228994
AndreaAguado <andrea95am@gmail.com>
2021-09-20 18:00:36 +0200

    fix counters in ex 2.4.1

--- modulo-3-leccion-02/modulo-3-leccion-02.4-ejercicio-01/src/components/App.js
+++ modulo-3-leccion-02/modulo-3-leccion-02.4-ejercicio-01/src/components/App.js
@@ -2,31 +2,25 @@ import '../styles/App.css';
 import {useState} from 'react';
 
 function App() {
+  let cont;
   const[value, setValue] = useState(0);
-  let cont = 0;
-  const adder = () =>{
-    cont++;
-    return cont;
-  }
   const adderHandle = (ev) => {
-    console.log(cont);
-    // cont++;
-    let newValue = adder();
-    
-    // console.log(cont);
-    // setValue(cont);
-    console.log(newValue);
-    setValue(newValue);
+    cont = value + 1;
+    return setValue(cont);
   } 
-  // const substractHandle = (ev) => {
-  //   cont--;
-  //   setValue(cont);
-  // }
+  const substractHandle = (ev) =>{
+    cont = value - 1;
+    return setValue(cont);
+  }
+  const reset = (ev) =>{
+    setValue(0);
+  } 
   return (
     <div>
       <h1>Contador: {value} </h1>
-      <button onClick={adderHandle}>Contador ++ </button>
-      {/* <button onClick={substractHandle}>Contador -- </button> */}
+      <button className="button" onClick={adderHandle}>Contador ++ </button>
+      <button className="button" onClick={substractHandle}>Contador - - </button>
+      <button className="button" onClick={reset}>Resetear</button>
     </div>
   );
 }
