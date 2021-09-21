1a84309799c0cd4dc529367e01c868ff6feb3350
AndreaAguado <andrea95am@gmail.com>
2021-09-21 22:22:58 +0200

    add ex 4.3.3 structure

--- modulo-3-leccion-04/modulo-3-leccion-04.3-ejercicio-3/src/components/App.js
+++ modulo-3-leccion-04/modulo-3-leccion-04.3-ejercicio-3/src/components/App.js
@@ -1,11 +1,88 @@
-import '../styles/App.css';
+// Fichero src/components/App.js
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  // Estados
+
+  const [series, setSeries] = useState([
+    { id: '123', name: 'Attack On Titan' },
+    { id: '456', name: 'Detective Conan' },
+    { id: '678', name: 'My Hero Academia' }
+  ]);
+  const [favorites, setFavorites] = useState([]);
+  const [searchName, setSearchName] = useState('');
+  const [searchIsFavorite, setSearchIsFavorite] = useState(false);
+
+  // Eventos
+
+  const handleFavorite = ev => {
+    const clickedSerieId = ev.currentTarget.id;
+    const foundSerie = series.find(serie => serie.id === clickedSerieId);
+    foundSerie.isFavorite = !foundSerie.isFavorite;
+    setFavorites([{...favorites, isFavorite: foundSerie.name}]);
+  };
+
+  const handleSearchName = ev => {
+    setSearchName(ev.target.value);
+  };
+
+  const handleSearchIsFavorite = ev => {
+    setSearchIsFavorite(ev.target.checked);
+  };
+
+  // Funciones de renderizado
+
+  const renderSeries = () => {
+    return (
+      series
+        // Filtramos por nombre
+        .filter(serie => {
+          return serie.name.toLowerCase().includes(searchName.toLowerCase());
+        })
+        // Filtramos por favorito
+        .filter(serie => {
+          if (searchIsFavorite === true) {
+            return serie.isFavorite === true;
+          } else {
+            return true;
+          }
+        })
+        // Mapeamos
+        .map(serie => {
+          return (
+            <li key={serie.id} id={serie.id} onClick={handleFavorite}>
+              <h2>{serie.name}</h2>
+              <p>Es mi serie favorita: {serie.isFavorite ? 'Sí' : 'No'}</p>
+            </li>
+          );
+        })
+    );
+  };
+
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <h1>Lista de series:</h1>
+
+      <ul>{renderSeries()}</ul>
+
+      <form>
+        <label htmlFor="searchName">Buscar por nombre de serie</label>
+        <input
+          type="text"
+          id="searchName"
+          value={searchName}
+          onChange={handleSearchName}
+        />
+        <label htmlFor="searchIsFavorite">Mostrar solo las favoritas</label>
+        <input
+          type="checkbox"
+          id="searchIsFavorite"
+          checked={searchIsFavorite}
+          onChange={handleSearchIsFavorite}
+        />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
