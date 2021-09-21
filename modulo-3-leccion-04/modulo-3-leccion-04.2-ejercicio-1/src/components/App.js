860250eeaeaee1b6cc3ac2c340b3f11c469ead34
AndreaAguado <andrea95am@gmail.com>
2021-09-21 21:43:56 +0200

    add ex 4.2.1

--- modulo-3-leccion-04/modulo-3-leccion-04.2-ejercicio-1/src/components/App.js
+++ modulo-3-leccion-04/modulo-3-leccion-04.2-ejercicio-1/src/components/App.js
@@ -1,9 +1,58 @@
 import '../styles/App.css';
+import {useState} from 'react';
 
 function App() {
+  // let completedClass = 'yes';
+  const [tasks, setTasks] = useState([
+    { task: 'Comprar harina, jamón y pan rallado', completed: true },
+    { task: 'Hacer croquetas ricas', completed: true },
+    { task: 'Ir a la puerta de un gimnasio', completed: false },
+    { task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio', completed: false }
+  ]);
+  
+  const renderTasksList = () =>{
+    return tasks.map( (task, index) => {
+      let listItem;
+      if(task.completed){
+        console.log(task.completed);
+         listItem = 
+         <li onClick={handleTask} 
+         className={'crossed ' + task.completed} 
+         id={index} 
+         key={index} 
+         >
+           {task.task}
+           </li>;
+      }
+      else {
+         listItem = 
+         <li onClick={handleTask} 
+         className={task.completed} 
+         id={index} 
+         key={index}>
+           {task.task}
+           </li>;
+      }
+      return listItem;   
+      
+    })
+  }
+
+  const handleTask = (ev) => {
+    const clickedId = ev.currentTarget.id;
+    console.log(clickedId);
+    //using clickedId as a position in the array of objects (since they match) the property 'completed' is accessed:
+    // tasks[clickedId].completed is the same as saying tasks[position in the array].completed (in this case)
+    tasks[clickedId].completed = ! tasks[clickedId].completed;
+    setTasks([...tasks]);
+  }
+
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <h1>Mi lista de tareas</h1>
+      <ol>
+        {renderTasksList()}
+      </ol>
     </div>
   );
 }
