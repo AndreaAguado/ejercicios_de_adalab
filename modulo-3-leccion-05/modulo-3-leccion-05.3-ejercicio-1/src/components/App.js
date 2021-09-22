b016a369b3d47562623a82f8d8a0a8abdce169ee
AndreaAguado <andrea95am@gmail.com>
2021-09-22 20:12:08 +0200

    add ex 5.3.1.

--- modulo-3-leccion-05/modulo-3-leccion-05.3-ejercicio-1/src/components/App.js
+++ modulo-3-leccion-05/modulo-3-leccion-05.3-ejercicio-1/src/components/App.js
@@ -1,11 +1,129 @@
+// Fichero src/components/App.js
 import '../styles/App.css';
+import { useEffect, useState } from 'react';
+// Importamos el servicio del local storage
+import ls from '../services/local-storage';
 
-function App() {
+// Si consoleas ls verás que es el objeto con las 4 funciones que hemos exportado en el servicio
+console.log(ls);
+
+const App = () => {
+  // Estados
+
+  // Obtenemos el nombre del local storage
+  // Si hay datos en el local storage esta función los devolverá
+  // Si no hay datos en el local storage esta función devolverá el segundo parámetro, es decir, un string vacío
+  const localStorageName = ls.get('name', '');
+  // Usamos localStorageName como parámetro de useState porque queremos que al arrancar la página name tenga lo que había en el local storage
+  const [name, setName] = useState(localStorageName);
+
+  // Hacemos lo mismo con el email pero en una sola línea
+  // Programarlo en una sola línea nos gusta más
+  const [email, setEmail] = useState(ls.get('email', ''));
+
+  const [tasks, setTasks] = useState(ls.get('tareas',[
+    { task: 'Comprar harina, jamón y pan rallado', completed: true },
+    { task: 'Hacer croquetas ricas', completed: true },
+    { task: 'Ir a la puerta de un gimnasio', completed: false },
+    { task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio', completed: false }
+  ] ));
+
+  // useEffect
+
+  // Usamos useEffect para guardar los datos en el local storage
+  // Cuando React renderice y pinte el HTML en la página ejecutará este useEffect
+  useEffect(() => {
+    // Guardamos el nombre, el email y las tareas en el local storage
+    ls.set('name', name);
+    ls.set('email', email);
+    ls.set('tareas', tasks);
+
+    // Este useEffect solo se ejecutará cuando cambie el nombre, el email o las tareas
+    console.log('Ha cambiado el nombre o el email');
+  }, [name, email, tasks]);
+
+  // Eventos
+
+  const handleName = ev => {
+    setName(ev.target.value);
+  };
+
+  const handleEmail = ev => {
+    setEmail(ev.target.value);
+  };
+
+  
+  const renderTasksList = () =>{
+    return tasks.map( (task, index) => {
+      let listItem;
+      if(task.completed){
+        console.log(task.completed);
+         listItem = 
+         <li onClick={handleTask} 
+         className={`crossed  ${ task.completed ? 'true' : 'false'}`} 
+         id={index} 
+         key={index} 
+         >
+           {task.task}
+           </li>;
+      }
+      else {
+         listItem = 
+         <li onClick={handleTask} 
+         className={task.completed ? 'true' : 'false'} 
+         id={index} 
+         key={index}>
+           {task.task}
+           </li>;
+      }
+      return listItem;   
+      
+    })
+  }
+
+  const handleTask = (ev) => {
+    const clickedId = ev.currentTarget.id;
+    console.log(clickedId);
+    //using clickedId as a position in the array of objects (since they match) the property 'completed' is accessed:
+    // tasks[clickedId].completed is the same as saying tasks[position in the array].completed (in this case)
+    tasks[clickedId].completed = ! tasks[clickedId].completed;
+    setTasks([...tasks]);
+  }
+
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <h1>Usando el local storage:</h1>
+
+      <form>
+        <label htmlFor="name">Escribe tu nombre:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Maricarmen"
+          value={name}
+          onChange={handleName}
+        />
+        <label htmlFor="email">Escribe tu email:</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="mari.carmen@gmail.com"
+          value={email}
+          onChange={handleEmail}
+        />
+      </form>
+
+      <h2>Tus datos son:</h2>
+      <p>Tu nombre es: {name}</p>
+      <p>Tu email es: {email}</p>
+      <h2>Mi lista de tareas</h2>
+      <ol>
+        {renderTasksList()}
+      </ol>
     </div>
   );
-}
+};
 
 export default App;
