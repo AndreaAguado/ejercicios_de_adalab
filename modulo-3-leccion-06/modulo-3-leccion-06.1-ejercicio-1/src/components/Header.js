b231e29681287fe0d53aae29e40d124719835cac
AndreaAguado <andrea95am@gmail.com>
2021-09-25 14:15:03 +0200

    import Menu component to Header component

--- modulo-3-leccion-06/modulo-3-leccion-06.1-ejercicio-1/src/components/Header.js
+++ modulo-3-leccion-06/modulo-3-leccion-06.1-ejercicio-1/src/components/Header.js
@@ -1,5 +1,6 @@
 import '../styles/App.css';
 import adalabLogo from '../images/adalab-logo-155x61.png';
+import Menu from './Menu';
 
 const Header = () => {
   return (
@@ -8,12 +9,7 @@ const Header = () => {
         <a target="_blank" href="https://adalab.es/" rel="noreferrer">
         <img title="Adalab" class="logo" src={adalabLogo} alt="logo adalab"/>
         </a>
-        <nav >
-            <ul class="links-container">
-                <li><a title="Blog" class="link" href="https://adalab.es/blog/" target="_blank" rel="noreferrer">Blog</a></li>
-                <li><a title="Contacto" class="link" href="./contact.html" target="_blank">Contacto</a></li>
-            </ul>
-        </nav>
+        <Menu></Menu>
         </header>
     </div>
   );
