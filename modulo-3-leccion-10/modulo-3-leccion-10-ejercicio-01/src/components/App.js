7b4db561f649b6f453653ba34692f9e569d55a3c
AndreaAguado <andrea95am@gmail.com>
2021-11-22 21:23:26 +0100

    add MenuItem and App

--- modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/components/App.js
+++ modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/components/App.js
@@ -1,5 +1,20 @@
+// Fichero src/components/App.js
+
+import MenuItem from './MenuItem';
+
 const App = () => {
-  return <div>Hola mundo</div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <MenuItem text="Blog" href="https://adalab.es/blog" openInNewTab />
+            <MenuItem text="Contacto" href="https://adalab.es/contacto" />
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default App;
