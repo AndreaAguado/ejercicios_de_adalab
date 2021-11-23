37e6ead8ac60357503fc76309dd973628dc762e8
AndreaAguado <andrea95am@gmail.com>
2021-11-23 13:52:47 +0100

    when getSquareTriangle(3,4) returns 6

--- modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/tests/area.test.js
+++ modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/tests/area.test.js
@@ -5,3 +5,10 @@ test('when getSquareArea(3) returns 9', () => {
     const result = area.getSquareArea(argument1);
     expect(result).toBe(9);
 })
+
+test('when getSquareTriangle(3,4) returns 6', () => {
+    const argument1 = 3;
+    const argument2 = 4;
+    const result = area.getSquareTriangle(argument1, argument2);
+    expect(result).toBe(6);
+})
