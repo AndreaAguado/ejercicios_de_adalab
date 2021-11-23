51cdbf5af8967df7b303afc91e16f9c3efcd839e
AndreaAguado <andrea95am@gmail.com>
2021-11-23 13:53:36 +0100

    change tests functions names

--- modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/tests/area.test.js
+++ modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/tests/area.test.js
@@ -1,12 +1,12 @@
 import area from '../services/area';
 
-test('when getSquareArea(3) returns 9', () => {
+test('getSquareArea(3) returns 9', () => {
     const argument1 = 3;
     const result = area.getSquareArea(argument1);
     expect(result).toBe(9);
 })
 
-test('when getSquareTriangle(3,4) returns 6', () => {
+test('getSquareTriangle(3,4) returns 6', () => {
     const argument1 = 3;
     const argument2 = 4;
     const result = area.getSquareTriangle(argument1, argument2);
