adc80668d520d9a49c5245bb3e9c6b4273226241
AndreaAguado <andrea95am@gmail.com>
2021-11-23 22:14:07 +0100

    add paddingLeft('xxxx', 6, 'x') devuelve 'xxxxxx' test

--- modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-05/src/tests/utils.test.js
+++ modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-05/src/tests/utils.test.js
@@ -26,3 +26,13 @@ test(`paddingLeft('ee', 4, 'aa') devuelve 'aaee'`, () => {
     const result = utils.paddingLeft(word, finalSize, elementForFilling);
     expect(result).toBe("aaee");
 })
+
+test(`paddingLeft('xxxx', 6, 'x') devuelve 'xxxxxx'`, () => {
+    const word = "xxxx";
+    const finalSize = 6;
+    const elementForFilling = "x";
+
+    const result = utils.paddingLeft(word, finalSize, elementForFilling);
+
+    expect(result).toBe("xxxxxx");
+})
