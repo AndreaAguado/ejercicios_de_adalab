61e521ad553f0a534175bb5e97bc4cd9cf98229b
AndreaAguado <andrea95am@gmail.com>
2021-11-23 15:00:14 +0100

    getSquareArea(false) returns -1 test

--- modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/services/area.js
+++ modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/services/area.js
@@ -1,7 +1,15 @@
 const getSquareArea = (side) => {
-    const area = side * side;
-    return area;
+    if (!side) {
+        const area = -1;
+        return area;
+    }
+    else if (side) {
+        const area = side * side;
+        return area;
+    }
+
 }
+
 const getSquareTriangle = (base, height) => {
     const area = base * height / 2;
     return area;
--- modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/tests/area.test.js
+++ modulo-3-leccion-10/modulo-3-leccion-10-ejercicio-01/src/tests/area.test.js
@@ -12,3 +12,10 @@ test('getSquareTriangle(3,4) returns 6', () => {
     const result = area.getSquareTriangle(argument1, argument2);
     expect(result).toBe(6);
 })
+
+test('getSquareArea(false) returns -1', () => {
+    const argument = false;
+    const result = area.getSquareArea(argument);
+    console.log(result);
+    expect(result).toBe(-1);
+})
